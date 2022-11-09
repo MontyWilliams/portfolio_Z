7ef864714ad05660a19abd20a7122638d7daef21
Montywilliams <3650@holbertonschool.com>
2022-11-09 15:44:04 -0600

    delete hardcoded ul: create an array of links and map throughe them instead

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,15 +3,38 @@ import {FaBars, FaTimes} from 'react-icons';
 
 const NavBar = () => {
 
+    const links = [
+        {
+            id: 1,
+            link: 'home'
+        },
+        {
+            id: 2,
+            link: 'about'
+        },
+        {
+            id: 3,
+            link: 'portfolio'
+        },
+        {
+            id: 4,
+            link: 'experience'
+        },
+        {
+            id: 5,
+            link: 'contact'
+        }
+    ]
+
     return (
         <div>
             <div className="flex justify-between items-center w-full h-20 fixed bg-black text-white">
                 <h1 className="text-5xl  font-signature ml-2" >Monty W</h1>
 
                 <ul className="flex">
-                    <li className="px-4 cursor-pointer capitalize hover:scale-125 duration-200">Home</li>
-                    <li className="px-4 cursor-pointer capitalize hover:scale-125 duration-200">Home</li>
-                    <li className="px-4 cursor-pointer capitalize hover:scale-125 duration-200">Home</li>
+                    {links.map(link => (
+                    <li  key={link.id} className="px-4 cursor-pointer capitalize hover:scale-125 duration-200">{link.link}</li>
+                    ))}
                 </ul>
             </div>
         </div>
