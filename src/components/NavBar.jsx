48f938d22b784d5ab4197f971cd7710dc7542933
Montywilliams <3650@holbertonschool.com>
2022-11-09 18:11:09 -0600

    add state for toggling navbar: add turnary to show diff icon for menu depending on state

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {FaBars, FaTimes} from 'react-icons';
+import React, { useState }from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
 
 const NavBar = () => {
 
@@ -26,16 +26,23 @@ const NavBar = () => {
         }
     ]
 
+    const [nav, setNav] = useState(false);
+
     return (
         <div>
             <div className="flex justify-between items-center w-full h-20 fixed bg-black text-white">
                 <h1 className="text-5xl  font-signature ml-2" >Monty W</h1>
 
-                <ul className="flex">
+                <ul className="hidden md:flex">
                     {links.map(link => (
                     <li  key={link.id} className="px-4 cursor-pointer capitalize hover:scale-125 duration-200">{link.link}</li>
                     ))}
                 </ul>
+
+                <div onClick={() => setNav(!nav)} className="cursor-pointer">
+                    {nav ? <FaTimes size={30}/>: <FaBars size={30}/>}
+                </div>
+                
             </div>
         </div>
     )
